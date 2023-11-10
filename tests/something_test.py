ba9ff56803cbd5a93b62833685ff65db3cf15151
VadimZhuckow <apex69knight@gmail.com>
2023-11-10 10:21:47 +0300

    add json validator

--- tests/something_test.py
+++ tests/something_test.py
@@ -9,7 +9,7 @@ def test_getting_posts():
     received_posts = response.json()
     print(received_posts)
     assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
-    assert len(received_posts) == 3, GlobalErrorMessages.WRONG_ELEMENT_COUNT.value
+    assert len(received_posts) == 4, GlobalErrorMessages.WRONG_ELEMENT_COUNT.value
 
     validate(received_posts, POST_SCHEMA)
 
